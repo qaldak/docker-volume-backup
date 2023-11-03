0104d7dee996ba33616ff6258a1fbdcb4f140f2c
Qaldak <qaldak@gmail.com>
2023-11-03 11:38:03 +0100

    implement init version: test_chat added

--- tests/notification/test_chat.py
+++ tests/notification/test_chat.py
@@ -1,11 +1,27 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+from slack_sdk.errors import SlackApiError
+
 from notification.chat import Slack
 
 
 class TestSlack(TestCase):
 
-    @patch("src.notification.chat.WebClient.chat_postMessage", return_value={'ok': True})
+    @patch("src.notification.chat.WebClient.chat_postMessage", return_value={'ok': True}, side_effect=None)
     def test_post_message_successful(self, res):
-        self.assertIsNone(Slack().post_message("Foobar"))
+        with self.assertLogs("notification.chat", level="INFO") as log:
+            self.assertIsNone(Slack().post_message("Foobar"))
+            self.assertEqual(
+                ["INFO:notification.chat:Message sent successfully"],
+                log.output)
+
+    @patch("src.notification.chat.WebClient.chat_postMessage",
+           side_effect=SlackApiError("auth failed", {'ok': False}))
+    def test_post_message_failed(self, res):
+        with self.assertLogs("notification.chat", level="ERROR") as log:
+            Slack().post_message("Foobar")
+            self.assertRaises(SlackApiError)
+            self.assertEqual(
+                ["ERROR:notification.chat:auth failed\n" "The server responded with: {'ok': False}"],
+                log.output)
