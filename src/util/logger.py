a94a3982df9b6cc5cccc32c66bd4b5c85d59e328
Qaldak <qaldak@gmail.com>
2023-11-25 21:13:14 +0100

    feat: log rotation for log file added

--- src/util/logger.py
+++ src/util/logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 
 class Logger:
@@ -7,10 +8,13 @@ class Logger:
     @staticmethod
     def init_logger(loglevel, container: str):
         logdir = os.getenv("LOG_DIR")
+        logfile = f"{container}-volume-backup.log"
+
+        loghandler = RotatingFileHandler(f"{logdir}/{logfile}", maxBytes=1049000, backupCount=1)
+        loghandler.setFormatter(
+            logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s'))
 
         if not os.path.isdir(logdir):
             os.makedirs(logdir)
 
-        # Todo: funcName vs name
-        logging.basicConfig(level=loglevel, filename=f"{logdir}/{container.replace('_', '-')}-volume-backup.log",
-                            format='%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
+        logging.basicConfig(level=loglevel, handlers=[loghandler])
