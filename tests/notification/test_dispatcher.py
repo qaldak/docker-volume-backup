5f90c5e5754923e98112344e6a7cc626c8261e06
Qaldak <qaldak@gmail.com>
2024-01-03 10:39:08 +0100

    Revert "feat: implement mqtt #39"
    This reverts commit 235cc39d207010427b34af486aaee86a46c3fc48.

--- tests/notification/test_dispatcher.py
+++ tests/notification/test_dispatcher.py
@@ -1,9 +1,8 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from notification.dispatcher import Dispatcher
+from notification.dispatcher import Dispatcher, Alerting, Receiver
 from util import cfg
-from util.accessor import Alerting, Receiver
 
 
 class TestDispatcher(TestCase):
